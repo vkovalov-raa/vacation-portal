9e6ad4f73844ab77aebfd563fcca6e104d50d6ee
Vladyslav Kovalov <vkovalov@rightandabove.com>
2025-06-26 14:00:59 +0300

    fix vacation manager

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="mt-20 text-center">
-    <h2 class="text-3xl font-bold">Добро пожаловать, {{ user?.name }}!</h2>
+    <h2 class="text-3xl font-bold">Welcome, {{ user?.name }}!</h2>
 
     <button @click="logout"
             class="mt-8 bg-gray-300 rounded px-4 py-2 hover:bg-gray-400">
-      Выйти
+      Exit
     </button>
   </div>
 </template>
