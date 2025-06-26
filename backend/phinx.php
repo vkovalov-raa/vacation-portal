813ceabafc8d98a3720186b86bb43637e994b022
Vladyslav Kovalov <vkovalov@rightandabove.com>
2025-06-26 19:17:42 +0300

    chore

--- backend/phinx.php
+++ backend/phinx.php
@@ -9,17 +9,15 @@ return [
     'default_environment' => 'development',
 
     'environments' => [
-        'environments' => [
-            'development' => [
-                'adapter' => 'mysql',
-                'host'    => $_ENV['DB_HOST'] ?? 'db',
-                'name'    => $_ENV['DB_DATABASE'] ?? 'vacation',
-                'user'    => $_ENV['DB_USERNAME'] ?? 'vacation',
-                'pass'    => $_ENV['DB_PASSWORD'] ?? 'vacation',
-                'port'    => 3306,
-                'charset' => 'utf8mb4',
-                'collation' => 'utf8mb4_unicode_ci',
-            ],
-        ]
+        'development' => [
+            'adapter' => 'mysql',
+            'host'    => $_ENV['DB_HOST'] ?? 'db',
+            'name'    => $_ENV['DB_DATABASE'] ?? 'vacation',
+            'user'    => $_ENV['DB_USERNAME'] ?? 'vacation',
+            'pass'    => $_ENV['DB_PASSWORD'] ?? 'vacation',
+            'port'    => 3306,
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+        ],
     ],
 ];
